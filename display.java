b2df1400c3a1dc82af8dbf66229252ea8e484509
Ayush00101 <ay84singh@gmail.com>
2026-08-22 21:11:25 +0530

    color coded and fixed a bug

--- display.java
+++ display.java
@@ -3,11 +3,9 @@ public class display {
     private static final String RESET = "\u001B[0m";
     private static final String RED = "\u001B[31m";
     private static final String GREEN = "\u001B[32m";
-    private static final String YELLOW = "\u001B[33m";
     private static final String BLUE = "\u001B[34m";
     private static final String MAGENTA = "\u001B[35m";
     private static final String CYAN = "\u001B[36m";
-    private static final String WHITE = "\u001B[37m";
 
 
     public static void show_coordinates(int side){
